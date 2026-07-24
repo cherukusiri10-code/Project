c3415f537aa6df38799ed0d7144914b98332936a
cherukusiri10-code <cherukusiri10@gmail.com>
2026-07-24 12:35:49 +0530

    Day-2
    C:\Users\cheru\OneDrive\Tasks\Task-2.py

---
+++ Task-2.py
@@ -0,0 +1,43 @@
+#Variables
+name = "Cheruku Siri"
+age = 19
+college = "GNITS"
+print("Name:", name)
+print("Age:", age)
+print("College:", college)
+
+#Datatypes
+name = "Siri"
+age = 19
+cgpa = 9.2
+is_student = True
+print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(is_student))
+
+#Operators
+a = 20
+b = 10
+print("Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
+print("Modulus =", a % b)
+
+#Loops
+print("Numbers from 1 to 5")
+for i in range(1, 6):
+    print(i)
+x= 1
+while x <= 5:
+    print(x)
+    x += 1
+
+#Functions
+def square(num):
+    return num * num
+result = square(6)
+print("Square =", result)
+
+print("Basic Python skills developed.")
